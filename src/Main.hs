1c194c152ead27bdf7f3df24dbb42b1eb7f8867d
zhonghaoxi <zhonghaoxi@yeah.net>
2016-12-11 21:45:17 +0800

    zhx is sad or not depends on whether gitigore has been changed

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 	import Parser
+	import Expr
+	import System.Environment
 
 	getASTTree s = Parser.parse s;
+
+	main :: IO()
+	main = do
+		line <- getLine
+		print (valueOfExpr (getASTTree line))
