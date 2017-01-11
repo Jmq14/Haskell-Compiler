215a29ef2ccdfacffe01341e7489b305db6f56b5
Jmq14 <jmq14@mails.tsinghua.edu.cn>
2017-01-11 10:38:30 +0800

    translation to python code is ok; but still there remains some problems 1.integer required 2. return expression in main() is redundant

--- Compiler/src/Translator.hs
+++ Compiler/src/Translator.hs
@@ -13,16 +13,49 @@ module Translator where
 
 	genParam :: [Variable.Variable] -> String
 	genParam [] = ""
+	genParam (x:[]) = show x 
 	genParam (x:xs) = show x ++ "," ++ genParam xs
 
+	genArguments :: [Expr.Expr] -> String
+	genArguments [] = ""
+	genArguments (x:[]) = genExpr x 
+	genArguments (x:xs) = genExpr x ++ ", " ++ genArguments xs
+
+-------------
 --Generate code of Tree Nodes
 	genNode :: (Int, Tree.Node) -> String
 
---StatementList	
+--Statement List	
 	genNode (indent, Tree.StatementListNode stmt next) = genNode (indent, stmt) ++ "\n" ++ genNode (indent, next)
 
 --Nil
 	genNode (indent, Tree.Nil) = "\n"
+
+--Error
+	genNode (indent, Tree.ErrorNode) = error "syntax error occurs!"
+
+--Set Variable
+	genNode (indent, Tree.SetVariableNode nowVariable expr) = 
+		replicate indent ' ' ++ show nowVariable ++ " = " ++ genExpr expr
+
+--While
+	genNode (indent, Tree.WhileNode condition statement) = 
+		replicate indent ' ' ++ "while " ++ genExpr condition ++ ":\n" ++ genNode (indent+4, statement)
+
+--If
+	genNode (indent, Tree.IfNode condition branch1 Tree.Nil) = 
+		replicate indent ' ' ++ "if " ++ genExpr condition ++ ":\n" ++ genNode (indent+4, branch1) 
+
+	genNode (indent, Tree.IfNode condition branch1 branch2) = 
+		replicate indent ' ' ++ "if " ++ genExpr condition ++ ":\n" ++ genNode (indent+4, branch1) ++ "\n" ++ replicate indent ' ' ++ "else:\n" ++ genNode (indent+4, branch2)
+
+--Make Vector
+	genNode (indent, Tree.MakeVectorNode var len) = 
+		replicate indent ' ' ++ show var ++ " = [None] * (" ++ genExpr len ++ ")"
+
+--Set Vectore
+	genNode (indent, Tree.VectorSetNode var idx value) =
+		replicate indent ' ' ++ show var ++ "[" ++ genExpr idx ++ "] = " ++ genExpr value  
 
 --Print
 	genNode (indent, Tree.PrintNode expr) = replicate indent ' ' ++ "print " ++ genExpr expr
@@ -30,6 +63,38 @@ module Translator where
 --Return 
 	genNode (indent, Tree.ReturnNode expr) = replicate indent ' ' ++ "return " ++ genExpr expr
 
+-------------
 --Generate code of Expressions
 	genExpr :: Expr.Expr -> String
+
+--Empty
+	genExpr (Expr.EmptyExpr) = ""
+
+--Constant
 	genExpr (Expr.NewConstant c) = show c
+
+--Expressions
+	genExpr (Expr.NewExpr Expr.NotOperator _ expr1 expr2) = "(not " ++ genExpr expr1 ++ ")"
+	genExpr (Expr.NewExpr Expr.AndOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " and " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.OrOperator _  expr1 expr2) = "(" ++ genExpr expr1 ++ " or " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.PlusOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " + " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.MinusOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " - " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.MultiplicationOperator _ expr1 expr2) = genExpr expr1 ++ " * " ++ genExpr expr2
+	genExpr (Expr.NewExpr Expr.DivisionOperator _ expr1 expr2) = genExpr expr1 ++ " / " ++ genExpr expr2
+	genExpr (Expr.NewExpr Expr.EqualOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " == " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.LessOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " < " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.LeqOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " <= " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.GreatOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " > " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.GeqOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " >= " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.ConsOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ ", " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.CarOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " , " ++ genExpr expr2 ++ ")"
+	genExpr (Expr.NewExpr Expr.CdrOperator _ expr1 expr2) = "(" ++ genExpr expr1 ++ " , " ++ genExpr expr2 ++ ")"
+
+--Function Call
+	genExpr (Expr.FunctionExpr function numVar params) = show function ++ "(" ++ genArguments params ++ ")"
+
+--Array Reference
+	genExpr (Expr.ArrayExpr array indx) = show array ++ "[" ++ genExpr indx ++ "]"
+
+
+
