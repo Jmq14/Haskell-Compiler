f8b6d6e8ee1c9acc4c5b48a1757271a66dd618de
zhonghaoxi <zhonghaoxi@yeah.net>
2017-01-15 15:16:10 +0800

    Great work has been done! Final step,quickcheck

--- Runner/test/Spec.hs
+++ Runner/test/Spec.hs
@@ -1,2 +1,22 @@
+import qualified KeyWord as KeyWord
+
+checklist = ["arr","fib","fib_arr","func1","fubc2","middle","qsort","qsort_big","queen","queen_fast","test1","test2","test3","test4"]
+
+testList [] = do
+	putStrLn (show KeyWord.keywords)
+	putStrLn "Code test done."
+
+testList (x:xs) =  do
+	putStrLn x
+	testList xs
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+	putStrLn "\n"
+	putStrLn "Test begin\n"
+	putStrLn "Code test begin:"
+	testList checklist
+	putStrLn ""
+	putStrLn "Property test begin:"
+	putStrLn "Property test done.\n"
+	putStrLn "Test done"
